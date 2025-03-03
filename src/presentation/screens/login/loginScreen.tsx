9523808e1552b7e54b827ec47f691183770af523
lopezcaleb <lopescaleb@gmail.com>
2025-03-03 12:32:39 -0500

    add validations forms

--- src/presentation/screens/login/loginScreen.tsx
+++ src/presentation/screens/login/loginScreen.tsx
@@ -1,6 +1,8 @@
 import { Layout, Text, Input, Button} from "@ui-kitten/components";
 import { Alert, Image, TouchableOpacity } from 'react-native'
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Formik } from "formik";
+import * as Yup from 'yup';
 import { useState } from "react";
 import { MyIcon } from "../../components/UI/MyIcon";
 import { useAuthStore } from "../../hooks/useLogin";
@@ -8,101 +10,124 @@ import { ScrollView } from "react-native-gesture-handler";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { RootStackParams } from "../../navigation/StackNavigation";
 
-
 export const LoginScreen = () => {
     const { top } = useSafeAreaInsets();
     const { login } = useAuthStore();
     const navigation = useNavigation<NavigationProp<RootStackParams>>();
 
-    const [username, setUsername] = useState('user_root');
-    const [password, setPassword] = useState('@Root123');
     const [isPosting, setPosting] = useState(false);
     const [security, setSecurity] = useState(true);
 
-    const onLogIn = async () => {
+    const PASSWORD_REGEX = new RegExp(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!.@#$%^&*])(?=.{8,})"
+      );
+
+    const validationSchema = Yup.object().shape({
+        username: Yup.string().min(6, 'Mínimo 6 caracteres').max(60, 'Maximo 60 caracteres').required('Campo Requerido'),
+        password: Yup.string()
+            .matches(
+            PASSWORD_REGEX,
+                "Debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial"
+            )
+            .required("La contraseña es requerida"),
+    });
+
+    const onLogIn = async ( value : { username: string, password: string} ) => {
         setPosting(true);
-        const wasSuccessFull = await login(username, password);
+        const wasSuccessFull = await login(value.username, value.password);
         setPosting(false);
         if(wasSuccessFull) return;
         Alert.alert('Error', 'Correo o contraseña incorrectos');
     }
 
     return (
-        <Layout style={{flex: 1, top: top}}>
-            <ScrollView style={{marginHorizontal: 40}}>
-            <Layout style={{flex: 1, top: 15}}>
-                <Image  
-                    source={require('../../../assets/logInImage.png')}
-                    style={{width: '100%', height: 200}}
-                />
+        <Formik
+            initialValues={{ username: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => onLogIn(values) }
+        >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
 
-                <Layout style={{flex: 1,  width: '100%', alignItems: 'center', top: 70}}> 
-                    <Text category='h3'>Bienvenido</Text>
+                <Layout style={{flex: 1, top: top}}>
+                    <ScrollView style={{marginHorizontal: 40}}>
+                        <Layout style={{flex: 1, top: 15}}>
+                            <Image  
+                                source={require('../../../assets/logInImage.png')}
+                                style={{width: '100%', height: 200}}
+                            />
 
-                    {/* Form */}
+                            <Layout style={{flex: 1,  width: '100%', alignItems: 'center', top: 70}}> 
+                                <Text category='h3'>Bienvenido</Text>
 
-                    <Layout style={{margin: 30, top: 10}}>
-                        <Input 
-                            label='Correo'
-                            style={{width: '100%'}}
-                            placeholder='Digite su correo'
-                            size='medium'
-                            value={username}
-                            onChangeText={(item) => setUsername(item)}
-                        />
+                                {/* Form */}
 
-                        <Layout style={{height: 30}}/>
+                                <Layout style={{margin: 30, top: 10}}>
+                                    <Input 
+                                        label='Correo'
+                                        style={{width: '100%'}}
+                                        placeholder='Digite su correo'
+                                        size='medium'
+                                        value={values.username}
+                                        onChangeText={handleChange('username')}
+                                    />
+                                    {errors.username && <Text style={{marginLeft: 10, color: '#ec9d9d'}}>{errors.username}</Text>}
 
-                        <Input
-                            label='Contrseña'
-                            placeholder='Digite su contraseña'
-                            size='medium'
-                            value={password}
-                            onChangeText={(item) => setPassword(item)}
-                            accessoryRight={
-                                <TouchableOpacity 
-                                    onPress={ () => setSecurity(!security) }
-                                >
-                                    <MyIcon name={security ? 'eye-off' : 'eye'} size={{ width: 25, height: 25 }}/>
-                                </TouchableOpacity >}
-                            secureTextEntry={security}
-                        />
+                                    <Layout style={{height: 30}}/>
 
-                        <Layout style={{height: 30}}/>
+                                    <Input
+                                        label='Contrseña'
+                                        placeholder='Digite su contraseña'
+                                        size='medium'
+                                        value={values.password}
+                                        onChangeText={handleChange('password')}
+                                        accessoryRight={
+                                            <TouchableOpacity 
+                                                onPress={ () => setSecurity(!security) }
+                                            >
+                                                <MyIcon name={security ? 'eye-off' : 'eye'} size={{ width: 25, height: 25 }}/>
+                                            </TouchableOpacity >}
+                                        secureTextEntry={security}
+                                    />
+                                    {errors.password && <Text style={{marginLeft: 10, color: '#ec9d9d'}}>{errors.password}</Text>}
 
-                        <Button
-                            onPress={onLogIn}
-                            disabled={isPosting}
-                        >
-                            <Text>Iniciar</Text>
-                        </Button>
+                                    <Layout style={{height: 30}}/>
 
-                        <Button
-                            appearance='ghost'
-                            onPress={ () => navigation.navigate('UserRegister') }
-                            disabled={isPosting}
-                        >
-                            <Text>Registrase</Text>
-                        </Button>
-                    </Layout>
+                                    <Button
+                                        onPress={() => handleSubmit()}
+                                        disabled={isPosting}
+                                    >
+                                        <Text>Iniciar</Text>
+                                    </Button>
 
-                    <Layout style={{height: 10}}/>
+                                    <Button
+                                        appearance='ghost'
+                                        onPress={ () => navigation.navigate('UserRegister') }
+                                        disabled={isPosting}
+                                    >
+                                        <Text>Registrase</Text>
+                                    </Button>
+                                </Layout>
 
-                    {/* Otros */}
+                                <Layout style={{height: 10}}/>
 
-                    <Text appearance='hint'>¿Olvidaste la contraseña?</Text>
+                                {/* Otros */}
 
-                    <Layout style={{height: 30}}/>
+                                <Text appearance='hint'>¿Olvidaste la contraseña?</Text>
 
-                    <Layout style={{ flexDirection: 'row' }}>
-                        <MyIcon name="facebook-outline"/>
-                        <MyIcon name="google-outline"/>
-                    </Layout>
+                                <Layout style={{height: 30}}/>
+
+                                <Layout style={{ flexDirection: 'row' }}>
+                                    <MyIcon name="facebook-outline"/>
+                                    <MyIcon name="google-outline"/>
+                                </Layout>
+                            </Layout>
+
+
+                        </Layout>
+                    </ScrollView>
                 </Layout>
+            )}
+        </Formik>
 
-
-                </Layout>
-            </ScrollView>
-        </Layout>
     )
 }
--- src/presentation/screens/userRegister/userRegister.tsx
+++ src/presentation/screens/userRegister/userRegister.tsx
@@ -1,9 +1,10 @@
-import { Input, Layout } from "@ui-kitten/components"
+import { Input, Layout, Text } from "@ui-kitten/components"
 import { MainLayout } from "../../layouts/MainLayout"
 import { ScrollView, Image, TouchableOpacity } from "react-native"
+import { Formik } from "formik"
+import * as Yup from 'yup';
 import { MyIcon } from "../../components/UI/MyIcon"
 import { useState } from "react"
-import { UserType } from "../../../domain/entities/user.type"
 import { MyButton } from "../../components/UI/MyButton"
 import { msUser } from "../../../api/msUser"
 import { MyAlert } from "../../components/UI/MyAlert"
@@ -14,15 +15,30 @@ export const UserRegister = () => {
     const navigation = useNavigation<NavigationProp<RootStackParams>>();
 
     const [security, setSecurity] = useState(true);
-    const [ user, setUser ] = useState<UserType>({
-        username: '',
-        email: '',
-        password: ''
-    });
 
-    const registerUser = async () => {
+        const PASSWORD_REGEX = new RegExp(
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!.@#$%^&*])(?=.{8,})"
+          );
+    
+        const validationSchema = Yup.object().shape({
+            username: Yup.string().min(6, 'Mínimo 6 caracteres').max(60, 'Maximo 60 caracteres').required('Campo Requerido'),
+            email: Yup.string().email('El correo no es valido').required('Requerido'),
+            password: Yup.string()
+                .matches(
+                PASSWORD_REGEX,
+                    "Debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial"
+                )
+                .required("La contraseña es requerida"),
+        });
+
+    const registerUser = async (
+        values: {
+            username: string;
+            email: string;
+            password: string;
+    }) => {
         try {
-            const res = await msUser.registerUser(user);
+            const res = await msUser.registerUser( values );
             if(res) MyAlert({ title: "Exito", subTitle: "Usuario registrado.", onSelectOk: () => navigation.navigate('LoginScreen') });
         } catch (error) {
             MyAlert({ title: "Error", subTitle: "El usuario no fue registrado.", onSelectOk: () => {} });            
@@ -30,57 +46,69 @@ export const UserRegister = () => {
     }
 
     return (
-        <MainLayout title="Registrar" subTitle="">
-            <ScrollView style={{marginHorizontal: 40}}>
-                <Layout style={{flex: 1}}>
-                    <Image  
-                        source={require('../../../assets/logInImage.png')}
-                        style={{width: '100%', height: 200}}
-                    />
-                    <Layout style={{flex: 1,  width: '100%', alignItems: 'center', top: 70}}> 
-                        <Input 
-                            placeholder="Nombre de usuario" 
-                            label="Usuario" 
-                            value={user.username}
-                            onChangeText={ item => setUser({...user, username: item}) }
-                        />
+        <Formik
+            initialValues={{ username: '', email: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => registerUser(values) }
+        >
 
-                        <Layout style={{height: 30}}/>
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
 
-                        <Input 
-                            placeholder="Direccion de correo"
-                            value={user.email} 
-                            label="Correo" 
-                            autoCapitalize="none" 
-                            keyboardType="email-address" 
-                            autoCorrect={false}
-                            onChangeText={ item => setUser({...user, email: item}) }
-                        />
-                        
-                        <Layout style={{height: 30}}/>
+                <MainLayout title="Registrar" subTitle="">
+                    <ScrollView style={{marginHorizontal: 40}}>
+                        <Layout style={{flex: 1}}>
+                            <Image  
+                                source={require('../../../assets/logInImage.png')}
+                                style={{width: '100%', height: 200}}
+                            />
+                            <Layout style={{flex: 1,  width: '100%', alignItems: 'center', top: 70}}> 
+                                <Input 
+                                    placeholder="Nombre de usuario" 
+                                    label="Usuario" 
+                                    value={values.username}
+                                    onChangeText={ handleChange('username') }
+                                />
+                                {errors.username && <Text style={{marginLeft: 10, color: '#ec9d9d'}}>{errors.username}</Text>}
 
-                        <Input
-                            label='Contrseña'
-                            placeholder='Digite su contraseña'
-                            size='medium'
-                            value={user.password}
-                            onChangeText={(item) => setUser({...user, password: item})}
-                            accessoryRight={
-                                <TouchableOpacity 
-                                    onPress={ () => setSecurity(!security) }
-                                >
-                                    <MyIcon name={security ? 'eye-off' : 'eye'} size={{ width: 25, height: 25 }}/>
-                                </TouchableOpacity >}
-                            secureTextEntry={security}
-                        />
-                        
-                        <Layout style={{height: 50}}/>
+                                <Layout style={{height: 30}}/>
 
-                        <MyButton text="Registrar" size="small" onSelect={ registerUser } />
+                                <Input 
+                                    placeholder="Direccion de correo"
+                                    value={values.email} 
+                                    label="Correo" 
+                                    autoCapitalize="none" 
+                                    keyboardType="email-address" 
+                                    autoCorrect={false}
+                                    onChangeText={ handleChange('email') }
+                                />
+                                {errors.email && <Text style={{marginLeft: 10, color: '#ec9d9d'}}>{errors.email}</Text>}
+                                
+                                <Layout style={{height: 30}}/>
 
-                    </Layout>
-                </Layout>
-            </ScrollView>
-        </MainLayout>
+                                <Input
+                                    label='Contrseña'
+                                    placeholder='Digite su contraseña'
+                                    size='medium'
+                                    value={values.password}
+                                    onChangeText={handleChange('password')}
+                                    accessoryRight={
+                                        <TouchableOpacity 
+                                            onPress={ () => setSecurity(!security) }
+                                        >
+                                            <MyIcon name={security ? 'eye-off' : 'eye'} size={{ width: 25, height: 25 }}/>
+                                        </TouchableOpacity >}
+                                    secureTextEntry={security}
+                                />
+                                {errors.password && <Text style={{marginLeft: 10, color: '#ec9d9d'}}>{errors.password}</Text>}
+                                
+                                <Layout style={{height: 50}}/>
+
+                                <MyButton text="Registrar" size="small" onSelect={ handleSubmit } />
+                            </Layout>
+                        </Layout>
+                    </ScrollView>
+                </MainLayout>
+            )}
+        </Formik>
     )
 }
